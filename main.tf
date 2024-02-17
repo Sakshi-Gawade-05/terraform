688962f83094838aaff98155e23d780b13e69647
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-18 00:09:50 +0530

    updated

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "ec2" {
     ami               = var.ami
     instance_type     = var.instance_type
     key_name          = var.key_name
-    security_group_ids   = module.vpc.aws_security_group_ids
+    aws_security_group_ids   = module.vpc.aws_security_group
     subnet_id         = module.vpc.aws_subnet
     project            = var.project
 }
