47ec7d2a0bdb8db2e8bb2db80ddf65d03c286c40
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-17 20:37:22 +0530

    module file is updated

---
+++ module/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "my_instance" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+  security_groups         =[var.security_groups]
+  subnet_id               =var.subnet_id 
+  tags ={
+    name = "var.project"
+  }  
+}
---
+++ variable.tf
@@ -0,0 +1,42 @@
+###vpc variables ###
+
+
+variable "vpc_cidr_block" {
+        default = "192.168.0.0/16"
+}
+
+variable "project" {
+        default = "dev"
+}
+
+
+variable "public_cidr_block" {
+        default = "192.168.0.0/24"
+}
+
+variable "private_cidr_block" {
+        default = "192.168.1.0/24"
+}
+
+
+variable "from_port" {
+        default = "80"
+}
+
+variable "to_port" {
+        default = "80"
+}
+
+### ec2 variables ###
+
+variable "ami" {
+    default = "ami-0f7cda165cc548085"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "key_name"  {
+    default = "ap-south-1"
+}
