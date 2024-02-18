024b08931a28930d2918cf61e0bf32b16c984b7f
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-18 22:59:24 +0530

    updated

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_instance" {
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
-  security_groupl       =[var.security_group_id]
+  security_groups         =[aws_security_group.my_security.id ]
   subnet_id               =var.subnet_id 
   tags ={
     Name = "var.project"
