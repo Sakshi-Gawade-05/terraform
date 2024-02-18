9a1a9943e5c2f599b86d4e0a69d2f6d8e54cd835
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-18 22:18:37 +0530

    updated

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_instance" {
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
-  security_group_ids        =[var.security_group_ids]
+  security_groupl       =[var.security_group_id]
   subnet_id               =var.subnet_id 
   tags ={
     Name = "var.project"
