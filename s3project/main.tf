9464caf633c0571253a116b018c12bb138ddd760
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-21 00:40:54 +0530

    updated

--- s3project/main.tf
+++ s3project/main.tf
@@ -3,3 +3,22 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
 }
+
+#need to define ownership of the bucket
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+#make the bucket public
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
