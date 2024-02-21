4b24c3106f7910cd4d389c1ccfbd68ea5d2bc6db
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-21 13:59:56 +0530

    updated

---
+++ 21-02/loops.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+    region "ap-south-1"
+}
+
+resource "aws_instance" "instance"{
+    count = "4"
+	ami = "ami-05674a88169b69e3b"
+	instance_type ="t3.micro"
+	key_name = "new-sao-paulo"
+    tags = {
+        Name = "my_instance"
+    }
+}
