4e5c0044771108bbea229599bcb78337f5942bfa
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-21 14:25:10 +0530

    updated

---
+++ 21-02/foreach.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "instance" {
+for_each = toset(var.ami_id)
+    ami = each.value
+    instance_type = "t2.micro"
+    key_name = "demo"
+    tags = {
+        Name = "for_each"
+    }
+}
+
+variable "ami_id" {
+   default = ["ami-0e670eb768a5fc3d4","ami-06b72b3b2a773be2b","ami-0918bbd8513a9aa3b", "ami-0918bbd8513a9aa3b"] 
+}
